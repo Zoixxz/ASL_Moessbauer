c6801f522954a8b72eddd4c0794d72b565709c27
Zoixxz <zoixxz@gmail.com>
2018-11-06 15:28:28 +0100

    file reader

--- evaluation_code.py
+++ evaluation_code.py
@@ -5,9 +5,30 @@ import os
 
 #loading files
 calibration = []
-for i in os.listdir():
-	if i.endswith('.txt'):
-		calibration.append(open(i))
+calibration_names = []
+paths = os.getcwd() + '/Fe10_90TestCallibration'
+
+def file_reader_sort(path, array):
+	"""
+	This function adds the values to the input array
+	no output!
+	"""
+	
+	#catch paths which end wih backslash
+	if path.endswith('/'):
+		return print('invalid path shape, remove the backslash at the end of the path')
+
+	__file_names = [] #internal use only, saves the names of the files
+	for i in os.listdir(path):
+		__file_names.append(i)
+	__file_names.sort()
+
+	for i in __file_names:
+		array.append(np.loadtxt(open(path + '/' + i)))
+	return
+
+file_reader_sort(paths, calibration)
+
 
 # configuration of the speaker with different aplitudes
 
